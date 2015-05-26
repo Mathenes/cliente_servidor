3daa9e295ffd4eba26493d77fb74720793bab9da
diegomachado <diegomachado@jimmipage>
2015-05-26 12:35:07 -0300

    Comentários removidos e Servidor alterado.

--- src/trabalho_sd/Leitura.java
+++ src/trabalho_sd/Leitura.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package trabalho_sd;
 
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author matheusenes
- */
 public class Leitura implements Runnable {
 
     Semaforo semaforo;
